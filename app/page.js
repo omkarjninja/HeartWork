d9811e0bb58704ce09ce7749067555cb573bb9f5
Omkar Jadhav <omkarjadhav@Omkars-MacBook-Air.local>
2024-10-14 01:42:56 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,91 @@
+"use client";
 import Image from "next/image";
-
+import { useState } from "react";
+import { db } from "../firebase/firebaseConfig";
+import { collection, addDoc ,serverTimestamp} from "firebase/firestore";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [career, setCareer] = useState("");
+  const [username, setUsername] = useState("");
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(career);
+    console.log(username);
+ 
+    const formData = {
+      career: career,
+      username: username || "Anonymous",
+      createdAt: serverTimestamp(), // username is optional
+    };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    try {
+      // Add a new document in Firestore
+      const docRef = await addDoc(collection(db, "careers"), formData);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
+
+    setCareer("");
+    setUsername("");
+    window.location.href="/passion";
+  }
+
+  return (
+    <>
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] backi">
+      <center>
+      <h1 className="text-4xl font-bold text-center font-black">If money didn’t exist, what career would light you up?</h1>
+      <p className="text-lg w-8/12 text-center pt-16 font-light">"What if your career wasn't about chasing dollars but chasing dreams? Picture yourself doing what you love every single day. No limits. No bills. Just pure joy and fulfillment. Dive into a world where your passion defines your work, not your paycheck!"</p>
+      
+
+<form onSubmit={submitHandler}>
+<div className="relative w-full  p-4">
+  <label htmlFor="career" className="sr-only"> Email </label>
+
+  <input
+    type="text"
+    id="career"
+    placeholder="Enter your dream career"
+    value={career}
+    className=" rounded-md border-gray-200  shadow-sm sm:text-sm p-5 border mt-10 lg:w-4/12 md:w-8/12 w-full font-bold"
+    onChange={(e) => setCareer(e.target.value)}
+    required
+  />
+
+ 
+</div>
+     
+
+<label
+  htmlFor="Username"
+  className="relative block rounded-md border border-gray-200 shadow-sm lg:w-4/12 md:w-8/12 w-full"
+>
+  <input
+    type="text"
+    id="Username"
+    value={username}
+    className="peer bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 p-4 w-full rounded-md lg:w-4/12 md:w-8/12 w-full"
+    placeholder="Username"
+    onChange={(e) => setUsername(e.target.value)}
+  />
+
+  <span
+    className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2  p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs"
+  >
+    Username (optional)
+  </span>
+</label>
+<button
+    type="submit"
+    id="submit"
+    className="rounded-md border-gray-200 shadow-sm sm:text-sm p-5 border mt-10 w-4/12 hover:scale-102 hover:shadow-xl transition lg:w-4/12 md:w-8/12 w-full" 
+  >Submit</button>
+   </form>
+      </center>
+     
     </div>
+
+    </>
   );
 }
---
+++ app/passion/page.js
@@ -0,0 +1,112 @@
+"use client";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase/firebaseConfig"; // adjust path as per your project structure
+import { collection, getDocs } from "firebase/firestore";
+import { format } from 'date-fns';
+export default function Passion() {
+    const [careers, setCareers] = useState([]);
+
+  useEffect(() => {
+    const fetchCareers = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "careers"));
+        const careersList = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setCareers(careersList);
+      } catch (e) {
+        console.error("Error fetching documents: ", e);
+      }
+    };
+    // console.log(careers[0].createdAt);
+    fetchCareers();
+  }, []);
+return(
+    <>
+   <div className="m-3">
+      <h1 className="m-3">Discover the careers , others dream of when money’s out of the picture.</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-3">
+        {careers.map((career) => ( 
+            <article 
+            className=" m-3 ml-3 hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover: hover:shadow-sm hover:[animation-duration:_4s] w-full bg-[url('https://images.beta.cosmos.so/3fa7f1c7-6cf0-4d0b-bff3-1ae62a16f9a8?format=jpeg')] bg-cover bg-center"
+          > 
+             <div className="rounded-[10px] p-4 !pt-20 sm:p-6">
+    <time datetime="2022-10-10" className="block text-xs text-white"> {career.createdAt && (
+              <span>
+                {" "}
+                {" "}
+                {format(career.createdAt.toDate(), "MMMM d, yyyy")}
+              </span>
+            )} </time>
+
+    <a href="#">
+      <h3 className="mt-0.5 text-lg font-medium text-white">
+      {career.career}
+      </h3>
+    </a>
+    <div className="mt-4 flex flex-wrap gap-1">
+      <span
+        className="whitespace-nowrap rounded-full bg-black px-3 py-1 text-xs text-white"
+      >
+        {   career.username}
+      </span>
+
+     
+    </div>
+         
+          </div>
+          </article>
+        ))}
+      </div>
+      {/*
+  Heads up! 👋
+
+theme: {
+  extend: {
+    animation: {    
+      border: 'background ease infinite',
+    },
+    keyframes: {
+      background: {
+        '0%, 100%': { backgroundPosition: '0% 50%' },
+        '50%': { backgroundPosition: '100% 50%' },
+      },
+    },
+  },
+}
+*/}
+
+{/* <article
+  className="hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] w-1/4"
+> */}
+  {/* <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
+    <time datetime="2022-10-10" className="block text-xs text-gray-500"> 10th Oct 2022 </time>
+
+    <a href="#">
+      <h3 className="mt-0.5 text-lg font-medium text-gray-900">
+        How to center an element using JavaScript and jQuery
+      </h3>
+    </a> */}
+
+    {/* <div className="mt-4 flex flex-wrap gap-1">
+      <span
+        className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+        Snippet
+      </span>
+
+      <span
+        className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+        JavaScript
+      </span>
+    </div> */}
+  {/* </div> */}
+{/* </article> */}
+    </div>
+    </>
+)
+}
+
+
